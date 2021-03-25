a94cb913c348cf72f58bbcacf5850a08c5c513ad
Marceline Ball <marchettiball@gmail.com>
2021-03-24 21:04:50 -0600

    write tests for calulating persent and anding round

--- test/Round-test.js
+++ test/Round-test.js
@@ -58,4 +58,26 @@ describe('Round', function() {
                                                correctAnswer: 'iteration method'
                                              })
   })
+
+  it('should be able to calculate the persentage of correct guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', '["object", "array", "function"]', 'object');
+    const card2 = new Card(3, 'What type of prototype method directly modifies the existing array?', '["mutator method", "accessor method", "iteration method"]', 'mutator method');
+    const card3 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', '["mutator method", "accessor method", "iteration method"]', 'iteration method');
+    const deck = new Deck(deck);
+    const round = new Round(deck);
+    round.takeTurn('object')
+    round.takeTurn('accessor method')
+    expect(round.calculatePercentCorrect().to.equal(50))
+  })
+
+  it('should be able to end the round', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', '["object", "array", "function"]', 'object');
+    const card2 = new Card(3, 'What type of prototype method directly modifies the existing array?', '["mutator method", "accessor method", "iteration method"]', 'mutator method');
+    const card3 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', '["mutator method", "accessor method", "iteration method"]', 'iteration method');
+    const deck = new Deck(deck);
+    const round = new Round(deck);
+    round.takeTurn('object')
+    round.takeTurn('accessor method')
+    expect(round.endRound()).to.equal(`** Round over! ** You answered ${round.calculatePercentCorrect()}% of the questions correctly!`)
+  })
 })
