4c6bcee866ede4507f3207d029cfbc83117356c7
Marceline Ball <marchettiball@gmail.com>
2021-03-25 20:13:03 -0600

    write remainder of tests for game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -11,22 +11,29 @@ describe('Game', function(){
   let game;
   beforeEach(function() {
     game = new Game()
-  })
-  it('should keep track of the current round', function() {
     game.start()
+  })
+
+  it('should keep track of the current round', function() {
     expect(game.currentRound).to.be.an.instanceof(Round)
   })
 
   describe('start method', function(){
 
     it('should create cards', function(){
-      game.start()
       expect(game.currentRound.deck.cards[0]).to.be.an.instanceof(Card)
     })
 
     it('should put cards in a deck', function(){
-      game.start()
       expect(game.currentRound.deck.cards.length).to.equal(30)
+    })
+
+    it('should invoke printMessage', function() {
+      expect(game.printMessage()).to.equal('Welcome to FlashCards! You are playing with 30 cards.')
+    })
+
+    it('should invoke printQuestion', function() {
+      expect(printQuestion()).to.equal(true)
     })
   })
 })
